6651a75e131c48f94ca46dfbca95b6dc1a7277ef
double Z <doubleZ0108@163.com>
2020-02-08 21:30:38 +0800

    刹车距离与车速的关系

--- SolveEquations/Algorithms/Genetic.py
+++ SolveEquations/Algorithms/Genetic.py
@@ -24,23 +24,31 @@ def f1(x):
 def f2(x):
     return x**3 - 2*x**2 + 1
 
-def Genetic(fx, *args):     # 函数作为函数的参数；动态参数
+def Genetic(fx, *args, max_times=100):
+    """
+    遗传算法计算零点
+    :param fx: 函数作为函数的参数
+    :param args: 动态参数（起始点）
+    :param max_times: 最大遗传次数
+    :return: 零点
+    """
     x0 = args[0]
     y0 = fx(x0)
 
     times = 0
-    while times < 100:
+    while times < max_times:
         sign = random.randint(1, 10)
         delta = random.random()/10.0    # 避免跳跃
         x1 = x0+delta if sign%2==0 else x0-delta
         y1 = fx(x1)
         if y1 < y0:
             x0, y0 = x1, y1
-        print('第',times,'次迭代, x = ', x0, ' y = ', y0)
+        # print('第',times,'次迭代, x = ', x0, ' y = ', y0)
         times += 1
+    return x0
 
 
 if __name__ == '__main__':
     x0 = 5
-    Genetic(f1, x0)
-    Genetic(f2, x0)
+    x_min = Genetic(f1, x0)
+    x_min = Genetic(f2, x0)
---
+++ SolveEquations/Examples/Braking Distance Speed/BrakingDistance_Speed.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+'''
+@program: BrakingDistance_Speed.py
+
+@description: 刹车距离与车速的关系
+
+@author: doubleZ
+
+@create: 2020/02/08 
+'''
+
+import numpy as np
+from SolveEquations.Algorithms.Genetic import Genetic
+
+data = np.loadtxt('data.txt')
+v, d = data[:,0], data[:,1]
+v2, v3, v4 = v**2, v**3, v**4
+
+# 使用数学理论方法直接求的k的最小值
+def mathematic():
+    k = np.sum(d*v2 - 0.75*v3)/np.sum(v4)
+    return k
+
+# 遗传算法所用的函数
+def f(k):
+    return np.abs(np.sum(d*v2 - 0.75*v3-k*v4))
+
+
+if __name__ == '__main__':
+
+    k = mathematic()
+    print('数学理论推导结果 ', k)
+
+    k = Genetic(f, 0, max_times=100)
+    print('遗传算法结果', k)
